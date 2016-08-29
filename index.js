95d55dfc99e5794bfe30c9bdd723a0ebad2555ae
Lauren Jacobson <lcj1188@gmail.com>
2016-08-29 09:50:58 -0400

    setup

---
+++ index.js
@@ -0,0 +1,9 @@
+//SRSV: A game in space, styled in the form of Battlestar Galactica
+
+var gamePrompt = require(game-prompt);
+var colors = require(colors);
+
+function intro () {
+  game
+}
+"You are the captain of a Solo Research Space Vehicle (S.R.S.V.) on an expedition to explore foreign planets. Your mission is to make contact with three alien life forms, acquire an artifact representative of their culture, and bring back your findings to Earth."
