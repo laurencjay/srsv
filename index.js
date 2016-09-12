6d131ebd7757f60fcb227c24434e1df63382bd44
Lauren Jacobson <lcj1188@gmail.com>
2016-09-12 16:59:17 -0400

    approach Aerilon

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function intro() {
     'You are the captain of the Battlestar Galactica, under the leadership ' +
     'of Commander William Adama on an expedition to find other living beings ' +
     'in space after the destruction of the Twelve Colonies.',
-    'You mission is to make contact with three groups of survivors, acquire food, Tylium, ' +
+    'You mission is to make contact with three groups of survivors on the colonies, acquire food, Tylium, ' +
     ' and tools, and make your way to settle on Earth.',
     'A voice comes over the intercom as you unlock the control room'.red,
     '"State your name for identification."'
@@ -108,6 +108,13 @@ function locationSelection() {
      fuelPrompt,
      travelPrompt
    ], travel);
+}
+
+function approachAerilon() {
+  gamePrompt([
+    'You have approached Aerilon, the agricultural heartland of the 12 Colonies',
+    'The planets resources seemed to be completely razed but you are still hopeful'.
+  ])
 }
 
 begin();
